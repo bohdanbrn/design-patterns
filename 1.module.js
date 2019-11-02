28562ae432b5a4ba3194bc5dd783a373778b3ebe
bohdanbrn <bohdanbrn@gmail.com>
2019-11-02 23:35:36 +0200

    module

---
+++ 1.module.js
@@ -0,0 +1,38 @@
+const BasketModule = (function() {
+    const goods = [];
+
+    return {
+        addProduct: function(product) {
+            goods.push(product);
+        },
+        getTotalPrice: function() {
+            let total = 0;
+
+            goods.forEach((item) => {
+                total += item.price;
+            });
+
+            console.log(total);
+        },
+        printProducts: function() {
+            for (let i = 0; i < goods.length; i++) {
+                console.log(goods[i].name, goods[i].price);
+            }
+        }
+    };
+}());
+
+const salt = {
+    name: "Salt",
+    price: 2
+};
+const bread = {
+    name: "Bread",
+    price: 1.5
+};
+
+BasketModule.addProduct(salt);
+BasketModule.addProduct(bread);
+
+BasketModule.printProducts();
+BasketModule.getTotalPrice();
