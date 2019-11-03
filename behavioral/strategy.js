2fa97b5d1fd63bbc66ebf9269be24b69dbae0285
bohdanbrn <bohdanbrn@gmail.com>
2019-11-03 22:38:48 +0200

    strategy

---
+++ behavioral/strategy.js
@@ -0,0 +1,47 @@
+function Fedex() {
+    this.calculate = (shippingPackage) => {
+        // Fedex calculations...
+        return 2.45;
+    };
+}
+
+function UPS() {
+    this.calculate = (shippingPackage) => {
+        // UPS calculations...
+        return 1.99;
+    };
+}
+
+function USPS() {
+    this.calculate = (shippingPackage) => {
+        // USPS calculations...
+        return 3.5;
+    };
+}
+
+function Shipping() {
+    this.company = "";
+
+    this.setStrategy = (company) => {
+        this.company = company;
+    };
+    this.calculate = shippingPackage => {
+        return this.company.calculate(shippingPackage);
+    };
+}
+
+const fedex = new Fedex();
+const ups = new UPS();
+const usps = new USPS();
+
+const shippingPackage = { from: "Alabama", to: "Georgia", weight: 1.56 };
+
+const shipping = new Shipping();
+shipping.setStrategy(fedex);
+console.log("Fedex: " + shipping.calculate(shippingPackage));
+
+shipping.setStrategy(ups);
+console.log("UPS: " + shipping.calculate(shippingPackage));
+
+shipping.setStrategy(usps);
+console.log("USPS: " + shipping.calculate(shippingPackage));
